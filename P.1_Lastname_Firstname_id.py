58421d3f2b3c1372af3ef36ad8d1a47a5150480d
Jacob Meyer <snakesos819@gmail.com>
2022-11-16 21:56:37 -0500

    11/16/22

--- P.1_Lastname_Firstname_id.py
+++ P.1_Lastname_Firstname_id.py
@@ -1,12 +1,37 @@
-def occurances_count():
-    pass
+def occur_count(word, lst):
+    count = 0
+    for i in range(len(lst)):
+        for j in range(len(lst[i])):
+            if word == lst[i][j]:
+                count += 1
+    return count
 
-def write_cvs():
-    infile = open("project.txt", "r")
+
+def create_lst(filename):
+    infile = open(filename, "r")
+    lst = []
     for line in infile:
-        for word in line:
-            pass
-    pass
+        lst += [line.split()]
+    infile.close()
+    print(lst)
+    return lst
+
+
+def write_cvs(filename, lst):
+    outfile = open(filename, "w")
+    for i in range(len(lst)):
+        for j in range(len(lst[i])):
+            word = lst[i][j]
+            occur = occur_count(word, lst)
+            line_num = i+1
+            word_num = j+1
+            print(word, occur, line_num, word_num)
+    outfile.close()
+
 
 def main():
-    pass
+    lst = create_lst("project.txt")
+    write_cvs("project.cvs", lst)
+
+
+main()
